3f96e2fd1ff3fd97353056746fc1d33c44ec1e73
mcAlexon <mr.alexdemonp2003@mail.ru>
2025-03-30 09:27:48 +0300

    правки по ревью

--- test/test_administration/test_actions_with_product.py
+++ test/test_administration/test_actions_with_product.py
@@ -1,31 +1,34 @@
 from pages.administration.admin_product_page import ProductAdminPage
 from pages.alerts import AlertPage
-from pages.administration.login_admin_page import login_admin_ui, LoginAdminPage
+from pages.administration.login_admin_page import login_admin_ui
+from pages.base_page import generate_random_data
 
-from faker import Faker
-import uuid
+import pytest
 
-f = Faker()
-uid = uuid.uuid4()
-name = f"Apple " + uid.variant
-title = uid.hex
-model = uid.hex
-keyword = uid.hex
+@pytest.fixture
+def product_admin_page(driver):
+    product_admin_page = ProductAdminPage(driver)
+    return product_admin_page
 
 
 class TestAdministration:
 
-    def test_open_catalog_products(self, driver, login_admin_ui):
-        product_admin_page = ProductAdminPage(driver)
+    def test_open_catalog_products(self, driver, login_admin_ui, product_admin_page):
         login_admin_ui
         product_admin_page.open_catalog_products()
 
         assert product_admin_page.checkout_products_page()
 
-    def test_create_new_product(self, driver, login_admin_ui):
+    def test_create_new_product(self, driver, login_admin_ui, product_admin_page, generate_random_data):
         """Добавление нового товара в разделе администратора"""
-        product_admin_page = ProductAdminPage(driver)
-        self.test_open_catalog_products(driver, login_admin_ui)
+
+        # Извлекаем данные из фикстуры
+        name = generate_random_data["name"]
+        title = generate_random_data["title"]
+        model = generate_random_data["model"]
+        keyword = generate_random_data["keyword"]
+
+        self.test_open_catalog_products(driver, login_admin_ui, product_admin_page)
         product_admin_page.click_add_new_button()
         product_admin_page.fill_form_product_tab_general(name=name, title=title)
         product_admin_page.change_tab_in_form_product(tab_name="Data")
@@ -36,10 +39,9 @@ class TestAdministration:
 
         assert AlertPage(driver).success_alert()
 
-    def test_delete_product(self, driver, login_admin_ui):
+    def test_delete_product(self, driver, login_admin_ui, product_admin_page):
         """Удаление товара из списка в разделе администратора"""
-        product_admin_page = ProductAdminPage(driver)
-        self.test_open_catalog_products(driver, login_admin_ui)
+        self.test_open_catalog_products(driver, login_admin_ui, product_admin_page)
         product_admin_page.delete_product()
 
         assert AlertPage(driver).success_alert()
--- test/test_opencart_user/test_main_page.py
+++ test/test_opencart_user/test_main_page.py
@@ -7,9 +7,10 @@ from pages.user_page import UserPage
 class TestsMain:
 
     def test_login_user(self, driver, db_connection):
-        UserPage(driver).open_user_page()
-        UserPage(driver).login(*create_random_user(db_connection))
-        UserPage(driver).wait_logged_in()
+        user_page = UserPage(driver)
+        user_page.open_user_page()
+        user_page.login(*create_random_user(db_connection))
+        user_page.wait_logged_in()
 
     def test_check_element_logo_from_main_page(self, driver):
         main_page = MainPage(driver)
